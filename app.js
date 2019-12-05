f5cab5f477782128cd381ee90236d2305dea78aa
Guillaume97490 <techer.guillaume97490@gmail.com>
2019-12-05 11:42:11 +0400

    add routes & controlers

--- app.js
+++ app.js
@@ -1,24 +1,27 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
+const mongoose = require('mongoose');
+const url = require('./database.js').url
+const stuffRoutes = require('./routes/stuff');
+
+const Thing = require('./models/thing');
+
+mongoose.connect(url,
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  console.log('Requête reçue !');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
 });
 
-app.use((req, res, next) => {
-  res.status(201);
-  next();
-});
-
-app.use((req, res, next) => {
-  res.json({ message: 'Votre requête a bien été reçue !' });
-  next();
-});
-
-app.use((req, res, next) => {
-  console.log('Réponse envoyée avec succès !');
-});
+app.use('/api/stuff', stuffRoutes);
 
 module.exports = app;
